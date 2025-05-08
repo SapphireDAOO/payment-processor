c847a4a63a7be0d127ed707a26237891aba74a91
Ademola <aademolaoladipo@gmail.com>
2025-05-08 19:41:57 +0100

    integrate escrow system and meta invoice payment flow into PaymentProcessorV2

--- src/PaymentProcessorV2.sol
+++ src/PaymentProcessorV2.sol
@@ -51,6 +51,8 @@ pragma solidity 0.8.28;
 // maybe allow them input the id ?
 
 import { SafeTransferLib } from "solady/utils/SafeTransferLib.sol";
+import { EscrowFactory } from "./EscrowFactory.sol";
+import { IEscrow } from "./interface/IEscrow.sol";
 
 // rewrite the data that are share between meta invoice and single invoice
 // single buyer
@@ -68,6 +70,8 @@ struct Invoice {
     uint256 createdAt;
     uint256 state;
     uint256 metaInvoiceId;
+    address paymentToken;
+    address escrow;
 }
 
 struct MetaInvoice {
@@ -75,29 +79,40 @@ struct MetaInvoice {
     uint256 upper;
     uint256 lower;
     address paymentToken;
+    address escrow;
 }
 
-contract PaymentProcessorV2 {
+contract PaymentProcessorV2 is EscrowFactory {
+    error InvalidBuyer();
+    error InvalidMetaPayment();
+    error InvalidPaymentToken();
+    error InvalidNativePayment();
+    error EscrowAddressMismatch();
+
     using SafeTransferLib for address;
 
     uint256 private nextInvoiceId;
     uint256 private nextMetaInvoiceId;
 
-    uint256 public constant INITIATED = 1;
-    uint256 public constant PAID = INITIATED + 1;
-    uint256 public constant ACCEPTED = PAID + 1;
-    uint256 public constant CANCELED = ACCEPTED + 1;
-    uint256 public constant REJECTED = CANCELED + 1;
-    uint256 public constant DISPUTED = REJECTED + 1;
-    uint256 public constant DISPUTE_RESOLVED = DISPUTED + 1;
-    uint256 public constant DISPUTE_DISMISSED = DISPUTE_RESOLVED + 1;
-    uint256 public constant DISPUTE_SETTLED = DISPUTE_DISMISSED + 1;
+    uint256 private feeRate;
+
+    uint32 public constant INITIATED = 1;
+    uint32 public constant PAID = INITIATED + 1;
+    uint32 public constant ACCEPTED = PAID + 1;
+    uint32 public constant CANCELED = ACCEPTED + 1;
+    uint32 public constant REJECTED = CANCELED + 1;
+    uint32 public constant DISPUTED = REJECTED + 1;
+    uint32 public constant DISPUTE_RESOLVED = DISPUTED + 1;
+    uint32 public constant DISPUTE_DISMISSED = DISPUTE_RESOLVED + 1;
+    uint32 public constant DISPUTE_SETTLED = DISPUTE_DISMISSED + 1;
+
+    uint256 public constant BASIS_POINTS = 10_000;
 
     mapping(uint256 id => Invoice data) private invoice;
-    mapping(uint256 id => MetaInvoice data) private metaInvoice;
+    mapping(uint256 metaInvoiceId => MetaInvoice data) private metaInvoice;
     mapping(address token => bool allowed) private isAllowed;
-    mapping(uint256 mId => mapping(uint256 id => Invoice)) private metaInvoiceToSubInvoice;
-    mapping(uint256 subInvId => uint256 metaInvId) private subInvoiceToMetaInvoiceId;
+    mapping(uint256 subInvoiceId => uint256 metaInvoiceId) private subInvoiceToMetaInvoiceId;
+    mapping(uint256 metaInvoiceId => mapping(uint256 subInvoiceId => Invoice invoices)) private metaInvoiceToSubInvoice;
 
     constructor() {
         nextInvoiceId = 1;
@@ -115,8 +130,8 @@ contract PaymentProcessorV2 {
         metaInv.lower = startInvoiceId;
         for (; i < sellers.length; i++) {
             startInvoiceId += i;
-
             totalPrice += prices[i];
+
             Invoice memory inv = _openInvoice(startInvoiceId, sellers[i], buyer, prices[i], thisMetaInvoiceId);
             metaInvoiceToSubInvoice[thisMetaInvoiceId][startInvoiceId] = inv;
             subInvoiceToMetaInvoiceId[startInvoiceId] = thisMetaInvoiceId;
@@ -135,34 +150,61 @@ contract PaymentProcessorV2 {
         _openInvoice(nextInvoiceId++, seller, buyer, price, 0);
     }
 
-    function payInvoice(uint256 id, address paymentToken) external payable {
-        if (paymentToken != address(0) && !isAllowed[paymentToken]) revert();
+    function paySingleInvoice(uint256 id, address paymentToken) external payable {
+        if (paymentToken != address(0) && !isAllowed[paymentToken]) revert InvalidPaymentToken();
         Invoice memory inv = invoice[id];
-        invoice[id].state = PAID;
-
-        _handlePayment(paymentToken, msg.value, inv.price);
+        _invoicePayment(inv, id, paymentToken);
+        invoice[id] = inv;
     }
 
     function payMetaInvoice(uint256 id, address paymentToken) external payable {
-        if (paymentToken != address(0) && !isAllowed[paymentToken]) revert();
+        if (paymentToken != address(0) && !isAllowed[paymentToken]) revert InvalidPaymentToken();
         MetaInvoice memory meta = metaInvoice[id];
-        if (msg.sender != metaInvoiceToSubInvoice[id][meta.lower].buyer) revert();
+
+        if (msg.value != meta.price) revert InvalidMetaPayment();
+        if (msg.sender != metaInvoiceToSubInvoice[id][meta.lower].buyer) revert InvalidBuyer();
 
         for (uint256 i = meta.lower; i <= meta.upper; i++) {
-            if (metaInvoiceToSubInvoice[id][i].state == CANCELED) continue;
-            metaInvoiceToSubInvoice[id][i].state = PAID;
+            Invoice memory inv = metaInvoiceToSubInvoice[id][i];
+            if (inv.state == CANCELED) continue;
+            _invoicePayment(inv, i, paymentToken);
+            metaInvoiceToSubInvoice[id][i] = inv;
+
             emit MetaInvoiceSubPaid(i);
         }
-
-        _handlePayment(paymentToken, msg.value, meta.price);
     }
 
-    function _handlePayment(address paymentToken, uint256 value, uint256 price) internal {
-        if (value != 0) {
-            if (value != price) revert();
-        } else {
-            paymentToken.safeTransferFrom(msg.sender, address(this), price);
+    function _invoicePayment(Invoice memory inv, uint256 id, address paymentToken) internal {
+        address predicted = getPredictedAddress(computeSalt(inv.seller, inv.buyer, id));
+
+        inv.state = PAID;
+        inv.escrow = predicted;
+
+        if (msg.value == 0) {
+            inv.paymentToken = paymentToken;
         }
+
+        uint256 feeValue = calculateFee(inv.price);
+
+        address actual = _handlePayment(paymentToken, id, msg.value, inv.price - feeValue);
+        IEscrow(actual).payFee(address(this), feeValue, feeValue);
+
+        if (actual != predicted) revert EscrowAddressMismatch();
+    }
+
+    function _handlePayment(address paymentToken, uint256 invoiceId, uint256 value, uint256 price)
+        internal
+        returns (address)
+    {
+        if (value > 0 && value != price) revert InvalidNativePayment();
+
+        address escrow = _create(msg.sender, invoiceId, value);
+
+        if (value == 0) {
+            paymentToken.safeTransferFrom(msg.sender, escrow, price);
+        }
+
+        return escrow;
     }
 
     function _openInvoice(uint256 id, address seller, address buyer, uint256 price, uint256 metaInvoiceId)
@@ -175,7 +217,9 @@ contract PaymentProcessorV2 {
             price: price,
             createdAt: block.timestamp,
             state: INITIATED,
-            metaInvoiceId: metaInvoiceId
+            metaInvoiceId: metaInvoiceId,
+            paymentToken: address(0),
+            escrow: address(0)
         });
 
         invoice[id] = inv;
@@ -183,15 +227,29 @@ contract PaymentProcessorV2 {
         return inv;
     }
 
+    // introduce escrow
+
     // cancel invoice
 
     // dispute invoice
 
     // resolve dispute
 
-    function getInvoice(uint256 id) external view returns (Invoice memory) {
+    function calculateFee(uint256 _amount) public view returns (uint256) {
+        return (_amount * feeRate) / BASIS_POINTS;
+    }
+
+    function setPaymentTokenState(address token, bool state) external {
+        isAllowed[token] = state;
+    }
+
+    function _getInvoice(uint256 id) internal view returns (Invoice memory) {
         uint256 metaInvoiceId = subInvoiceToMetaInvoiceId[id];
         return metaInvoiceId == 0 ? invoice[id] : metaInvoiceToSubInvoice[metaInvoiceId][id];
+    }
+
+    function getInvoice(uint256 id) external view returns (Invoice memory) {
+        return _getInvoice(id);
     }
 
     function getMetaInvoice(uint256 id) external view returns (MetaInvoice memory) {
