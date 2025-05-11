706b3920f13ef083330b46cc943d876fe39bba27
Ademola <aademolaoladipo@gmail.com>
2025-05-11 20:13:59 +0100

    remove inline planning comments from PaymentProcessorV2

--- src/PaymentProcessorV2.sol
+++ src/PaymentProcessorV2.sol
@@ -1,67 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-// -> specific tokens are allowed
-// -> introduce SafeTransferLib
-
-// auth
-// -> create for
-// -> create dispute
-// -> marketplace sets new dispute state/amount
-
-// users
-// allow single orders too i.e one-to-one
-// -> multiple order, multiple seller single buyer i.e one-to-many
-// -> how to handler multiple orders?
-// -> the sellers address will be entered
-// -> fill in order neccasary data(state, price etc)
-// data includes sellers address, invoice id, time created, price
-
-// Types of orders
-// Single order
-// Bulk order
-// What would the data structure look like ?
-// Id mapped to the invoice data for single order
-// what about Bulk/multiple orders ?
-// Have a bulk order Id, unique to only bulk order, then order sub invoice should be linked to itr
-
-// when seller accepts orders, there n number of days
-// possible scenarios: acceptance, dispute, cancel
-// accepts :-> escrow for n days
-// cancels :-> refunded. Even in a bulk order sellers buyer can decide to cancel specific ones
-
-// To-do
-// Create invoice for user
-// handle single invoice
-
-// STATES ?
-// INITIATED
-// ACCEPTED
-// CANCELED
-// REJECTED
-// DISPUTED
-// DISPUTE RESOLVED
-// DISPUTE DISMISSED
-// DISPUTE SETTLED
-
-// Handling meta invoice
-// single seller multiple items
-// multiple seller multiple items
-
-// maybe allow them input the id ?
-
 import { SafeTransferLib } from "solady/utils/SafeTransferLib.sol";
 import { EscrowFactory } from "./EscrowFactory.sol";
 import { IEscrow } from "./interface/IEscrow.sol";
-
-// rewrite the data that are share between meta invoice and single invoice
-// single buyer
-// all price packed
-// single creation time
-
-// diff seller
-// diff price
-// diff state
 
 struct Invoice {
     address seller;
